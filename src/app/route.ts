c5c8a0ce40a05ac60272304ccd8bb75e26f0594e
Bebelin <63244054+Sevaaas1@users.noreply.github.com>
2025-06-05 23:19:41 -0400

    Mezcla apps

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,8 +1,20 @@
 import { NextResponse } from "next/server";
 import {v4 as uuidv4} from 'uuid';
 let datos = [
-  { id: uuidv4(), usuario: "Martín" },
-];
-export async function GET(request:Request) {
-return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+  {
+    id:uuidv4(),
+    respuestas:{
+      item:["si","no","perro"]
+    }
+  }
+]
+export async function GET() {
+  return NextResponse.json( datos );
+}
+
+export async function POST() {
+
+    const nuevoUsuario = { id: uuidv4(), respuestas:{item:["no","ne","na"]} };
+    datos.push(nuevoUsuario);
+    return NextResponse.json(nuevoUsuario);
 }
