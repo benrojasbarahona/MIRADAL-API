88b1fa03cd3c1d733bc740a9efa526633a4062d0
Bebelin <63244054+Sevaaas1@users.noreply.github.com>
2025-06-24 23:48:04 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {Add commentMore actions
+  async headers() {
     return [
       {
         source: "/obtenerRespuestas/:path*",
